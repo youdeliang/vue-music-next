8cac2f3ce991834cedbbcd1b2afbfe29ae2219ce
游德梁 <midiexiang@i9i8.com>
2021-11-22 23:57:51 +0800

    feat: init demo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <m-header></m-header>
+</template>
+
+<script>
+import Header from '@/components/header/header'
+
+export default {
+  components: {
+    MHeader: Header,
+  },
+}
+</script>
