f316a4262498cbd4af43e77ff756cb9a23795a20
chenqin <chenqin>
2021-11-30 18:02:32 +0800

    2-8

--- src/components/base/slider/slider.vue
+++ src/components/base/slider/slider.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="slider" ref="rootRef">
+  <div
+    class="slider"
+    ref="rootRef"
+  >
     <div class="slider-group">
-      <div class="slider-page" v-for="item in sliders" :key="item.id">
+      <div
+        class="slider-page"
+        v-for="item in sliders"
+        :key="item.id"
+      >
         <a :href="item.link">
           <img :src="item.pic" />
         </a>
@@ -20,20 +27,23 @@
 
 <script>
 import { ref } from 'vue'
+import useSlider from './use-slider'
 export default {
   name: 'slider',
   props: {
     sliders: {
       type: Array,
-      default() {
+      default () {
         return []
       },
     },
   },
-  setup() {
+  setup () {
     const rootRef = ref(null)
+    const { currentPageIndex } = useSlider(rootRef)
     return {
       rootRef,
+      currentPageIndex
     }
   },
 }
@@ -42,8 +52,8 @@ export default {
 <style lang="scss" scoped>
 .slider {
   min-height: 1px;
-  font-size: 0;
-  touch-action: pan-y;
+  font-size: 0; //?
+  touch-action: pan-y; //?
   .slider-group {
     position: relative;
     overflow: hidden;
--- src/components/base/slider/use-slider.js
+++ src/components/base/slider/use-slider.js
@@ -1,5 +1,34 @@
 import BScroll from "@better-scroll/core";
 import Slide from "@better-scroll/slide";
-export default function useSlider(wrapperRef){
-  
+import { onMounted, onUnmounted, ref } from "@vue/runtime-core";
+
+BScroll.use(Slide)
+export default function useSlider (wrapperRef) {
+    const slider = ref(null)
+
+    const currentPageIndex = ref(0)
+
+    onMounted(() => {
+        const sliderVal = slider.value = new BScroll(wrapperRef.value, {
+            click: true,
+            scrollX: true,
+            scrollY: false,
+            momentum: false,
+            bounce: false, //
+            probeType: 2, //
+            slide: true
+        })
+
+        sliderVal.on('slideWillChange', (page) => {
+            currentPageIndex.value = page.pageX
+        })
+    })
+
+    // 卸载
+    onUnmounted(() => {
+        slider.value.destroy()
+    })
+
+    return { slider, currentPageIndex }
+
 }
